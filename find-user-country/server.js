28979d99a7aed625a19762905a2068168211b640
Lisnyi <russelhobbbs@gmail.com>
2023-11-14 21:10:57 +0200

    add IP mask

--- find-user-country/server.js
+++ find-user-country/server.js
@@ -2,6 +2,6 @@ import { app } from "./app.js";
 
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
